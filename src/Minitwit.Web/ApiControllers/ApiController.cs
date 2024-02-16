d79aff96fb0792f75c5486ee8cf931b3afb2d375
olau <olau@itu.dk>
2024-02-16 23:19:37 +0100

    Added the necessary endpoints
    Co-author: ChatGPT

--- src/Minitwit.Web/ApiControllers/ApiController.cs
+++ src/Minitwit.Web/ApiControllers/ApiController.cs
@@ -1,15 +1,71 @@
-namespace Minitwit.Web.ApiControllers
+using Microsoft.AspNetCore.Mvc;
+
+namespace Minitwit.Web.ApiControllers;
 
 
-{
+
     /*
      * Endpoints that needs to be exposed:
-     * "/latest"
-     * "/register"
-     * "/msgs"
-     * "/msgs/<username>"
-     * "/fllws/<username>"
+     * "/latest" GET
+     * Should return the latest value
+     * 
+     * "/register" POST
+     * err: 400, Suc: 204
+     * "/msgs" GET
+     * 
+     * "/msgs/<username>" GET POST
+     * Abort: 404, Suc: 204
+     * "/fllws/<username>" GET POST
      */
     
-    
-}
+    [Route("api")]
+    [ApiController]
+    public class ApiController : ControllerBase
+    {
+        
+        [HttpGet("latest")]
+        public IActionResult GetLatest()
+        {
+            
+            return Ok();
+        }
+
+        
+        [HttpPost("register")]
+        public IActionResult RegisterUser()
+        {
+           
+            return Ok();
+        }
+
+        
+        [HttpGet("msgs")]
+        public IActionResult GetAllMessagesFromPublicTimeline()
+        {
+           
+            return Ok("All messages");
+        }
+        
+
+       
+        [HttpGet("msgs/{username}")]
+        public IActionResult GetUserMessages(string username)
+        {
+            return Ok();
+        }
+
+        
+        [HttpGet("fllws/{username}")]
+        public IActionResult GetUserFollowers(string username)
+        {
+            return Ok(); 
+            
+        }
+        
+        [HttpPost("fllws/{username}")]
+        public IActionResult FollowerUser()
+        {
+            return Ok(); 
+            
+        }
+    }
