0c7dd9ab68289d9914808f23772436c31f2e2138
olau <olau@itu.dk>
2024-04-12 12:35:52 +0200

    Added NUnit test project to hold End2End test

---
+++ test/Minitwit.End2EndTest/UnitTest1.cs
@@ -0,0 +1,15 @@
+namespace Minitwit.End2EndTest;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
