c98e02dbbaece2b666309b856a66fabd317a9663
olau <olau@itu.dk>
2024-04-15 13:13:11 +0200

    Some changes(NOT WORKING)

--- test/Minitwit.End2EndTest/RegisterE2ETest.cs
+++ test/Minitwit.End2EndTest/RegisterE2ETest.cs
@@ -11,6 +11,11 @@ public class RegisterE2ETest : PageTest
     private IBrowser _browser;
     private IPage _page;
     
+    private readonly Dictionary<string, string> _user =
+        new() {{"username", "TestUser123"}, {"email", "TestEmail@test.test"}, {"password", "TestPassword"}, {"CheepText", "Hello fellow students! 4283"}};
+    private readonly string _publicUrl = "http://localhost:8080/";
+    private readonly string _registerUrl = "http://localhost:8080/Identity/Account/Register";
+    private readonly string _loginUrl = "http://localhost:8080/Identity/Account/Login";
     
     [SetUp]
     public async Task SetUpAsync()
@@ -18,7 +23,8 @@ public class RegisterE2ETest : PageTest
         _playwright = await Microsoft.Playwright.Playwright.CreateAsync();
         _browser = await _playwright.Chromium.LaunchAsync(new BrowserTypeLaunchOptions
         {
-            //Headless = false
+            Headless = false,
+            SlowMo = 2000
         });
         _page = await _browser.NewPageAsync();
         
@@ -28,21 +34,48 @@ public class RegisterE2ETest : PageTest
     [Test]
     public async Task RegisterUserViaGuiTestWithCredentials()
     {
-         await _page.GotoAsync("http://localhost:8080/Identity/Account/Register");
+         await _page.GotoAsync(_registerUrl);
          
-         await _page.GetByPlaceholder("username").FillAsync("TestUser123");
+         await _page.GetByPlaceholder("username").FillAsync(_user["username"]);
         
-         await _page.GetByPlaceholder("name@example.com").FillAsync("TestEmail@test.test");
+         await _page.GetByPlaceholder("name@example.com").FillAsync(_user["email"]);
          
-         await _page.FillAsync("input[id='Input_Password']", "TestPassword");
-         await _page.FillAsync("input[id=Input_ConfirmPassword]", "TestPassword");
+         await _page.FillAsync("input[id='Input_Password']", _user["password"]);
+         await _page.FillAsync("input[id=Input_ConfirmPassword]", _user["password"]);
          
-         //await _page.ClickAsync("button[type=submit]");
+         await _page.ClickAsync("button[type=submit]");
          
          
-         //await _page .WaitForNavigationAsync();
-         //await Expect(_page).ToHaveURLAsync("http://localhost:8080/");
+         await _page .WaitForURLAsync(_publicUrl);
+         await Expect(_page).ToHaveURLAsync(_publicUrl);
+    }
+    [Test]
+    public async Task MakeCheepUsingRegisteredUser()
+    {
+        await _page.GotoAsync(_publicUrl);
+        
+        await Expect(_page.GetByRole(AriaRole.Heading, new () 
+            {Name = "What's on your mind " + _user["username"] + "?"})).ToBeVisibleAsync();
+        
+        await Expect(Page.GetByText("Write your text here")).ToBeVisibleAsync();
+        
+        await Page.GetByLabel("Write your text here").FillAsync(_user["CheepText"]);
+        await Page.GetByRole(AriaRole.Button, new() { Name = "Cheep" }).ClickAsync();
+        
+        await Expect(_page).ToHaveURLAsync(_publicUrl + _user["username"]);
+        
+        await Expect(_page.GetByRole(AriaRole.Heading, new () {Name = "TestUser123's Timeline"}))
+            .ToBeVisibleAsync();
+        await Expect(_page.GetByText(_user["CheepText"])).ToBeVisibleAsync();
 
+    }
+
+    [Test]
+    public async Task CheepShouldBeVisibleInPublic()
+    {
+        await _page.GotoAsync(_publicUrl);
+        
+        await Expect(_page.GetByText(_user["CheepText"])).ToBeVisibleAsync();
     }
 
     [TearDown]
--- test/Minitwit.End2EndTest/UITest/UINotLoggedIn.cs
+++ test/Minitwit.End2EndTest/UITest/UINotLoggedIn.cs
@@ -3,11 +3,16 @@ using Microsoft.Playwright.NUnit;
 
 namespace Minitwit.End2EndTest.UITest;
 
+[Parallelizable(ParallelScope.Self)]
+[TestFixture]
 public class UiNotLoggedIn : PageTest
 {
     private IPlaywright _playwright;
     private IBrowser _browser;
     private IPage _page;
+    private readonly string _publicUrl = "http://localhost:8080/";
+    private readonly string _registerUrl = "http://localhost:8080/Identity/Account/Register";
+    private readonly string _loginUrl = "http://localhost:8080/Identity/Account/Login";
     
     [SetUp]
     public async Task SetUpAsync()
@@ -15,8 +20,8 @@ public class UiNotLoggedIn : PageTest
         _playwright = await Microsoft.Playwright.Playwright.CreateAsync();
         _browser = await _playwright.Chromium.LaunchAsync(new BrowserTypeLaunchOptions
         {
-            //Headless = false,
-            //SlowMo = 2000
+            Headless = false,
+            SlowMo = 2000
         });
         _page = await _browser.NewPageAsync();
         
@@ -25,7 +30,7 @@ public class UiNotLoggedIn : PageTest
     [Test]
     public async Task CheckPublicTimeline()
     {
-            await _page.GotoAsync("http://localhost:8080/");
+            await _page.GotoAsync(_publicUrl);
             
             //Check Navbar
             await Expect(_page.GetByRole(AriaRole.Link, new() { Name = "Public Timeline" })).ToBeVisibleAsync();
@@ -43,43 +48,44 @@ public class UiNotLoggedIn : PageTest
     [Test]
     public async Task CheckNavBarReRouting()
     {
-        await _page.GotoAsync("http://localhost:8080/");
+        await _page.GotoAsync(_publicUrl);
 
         await _page.GetByRole(AriaRole.Link, new() { Name = "Login" }).ClickAsync();
-        await Expect(_page).ToHaveURLAsync("http://localhost:8080/Identity/Account/Login");
+        await Expect(_page).ToHaveURLAsync(_loginUrl);
         
         await _page.GetByRole(AriaRole.Link, new() { Name = "Public timeline" }).ClickAsync();
-        await Expect(_page).ToHaveURLAsync("http://localhost:8080/");
+        await Expect(_page).ToHaveURLAsync(_publicUrl);
         
         await _page.GetByRole(AriaRole.Link, new() { Name = "Register" }).ClickAsync();
-        await Expect(_page).ToHaveURLAsync("http://localhost:8080/Identity/Account/Register");
+        await Expect(_page).ToHaveURLAsync(_registerUrl);
         
     }
     [Test]
     public async Task CheckRegister()
     {
-        await _page.GotoAsync("http://localhost:8080/Identity/Account/Register");
+        await _page.GotoAsync(_registerUrl);
         
-        Expect( _page.GetByRole(AriaRole.Link, new() { Name = "Register" }));
-        Expect (_page.GetByRole(AriaRole.Heading, new() { Name = "Register", Exact = true }));
-        Expect (_page.GetByRole(AriaRole.Heading, new() { Name = "Create a new account." }));
-        Expect (_page.GetByText("Username"));
-        Expect (_page.Locator("#registerForm div").Filter(new() { HasText = "Email" }));
-        Expect (_page.GetByText("Password", new() { Exact = true }));
-        Expect (_page.GetByText("Confirm Password"));
+        await Expect( _page.GetByRole(AriaRole.Link, new() { Name = "Register" })).ToBeVisibleAsync();
+        
+        await Expect (_page.GetByRole(AriaRole.Heading, new() { Name = "Register", Exact = true })).ToBeVisibleAsync();
+        await Expect (_page.GetByRole(AriaRole.Heading, new() { Name = "Create a new account." })).ToBeVisibleAsync();
+        await Expect (_page.GetByText("Username")).ToBeVisibleAsync();
+        await Expect (_page.Locator("#registerForm div").Filter(new() { HasText = "Email" })).ToBeVisibleAsync();
+        await Expect (_page.GetByText("Password", new() { Exact = true })).ToBeVisibleAsync();
+        await Expect (_page.GetByText("Confirm Password")).ToBeVisibleAsync();
         
     }
 
     [Test]
     public async Task CheckLogin()
     {
-        await _page.GotoAsync("http://localhost:8080/Identity/Account/Login");
+        await _page.GotoAsync(_loginUrl);
         
-        Expect (_page.GetByRole(AriaRole.Link, new() { Name = "Login" }));
-        Expect (_page.GetByRole(AriaRole.Heading, new() { Name = "Log in", Exact = true }));
-        Expect (_page.GetByRole(AriaRole.Heading, new() { Name = "Use a local account to log in." }));
-        Expect (_page.GetByText("Username"));
-        Expect (_page.GetByText("Password", new() { Exact = true })); 
+        await Expect (_page.GetByRole(AriaRole.Link, new() { Name = "Login" })).ToBeVisibleAsync();
+        await Expect (_page.GetByRole(AriaRole.Heading, new() { Name = "Log in", Exact = true })).ToBeVisibleAsync();
+        await Expect (_page.GetByRole(AriaRole.Heading, new() { Name = "Use a local account to log in." })).ToBeVisibleAsync();
+        await Expect (_page.GetByText("Username")).ToBeVisibleAsync();
+        await Expect (_page.GetByText("Password", new() { Exact = true })).ToBeVisibleAsync(); 
     }
     
 
