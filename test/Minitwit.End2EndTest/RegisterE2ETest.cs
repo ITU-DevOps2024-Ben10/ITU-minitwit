b82e0dbc52f9d036cd28db9da0fd3a412d89d900
olau <olau@itu.dk>
2024-04-19 10:25:35 +0200

    Added DeleteUser to test, as cleanup method

--- test/Minitwit.End2EndTest/RegisterE2ETest.cs
+++ test/Minitwit.End2EndTest/RegisterE2ETest.cs
@@ -11,11 +11,12 @@ public class RegisterE2ETest : PageTest
     private IBrowser _browser;
     private IPage _page;
     
-    private readonly Dictionary<string, string> _user =
+    private static readonly Dictionary<string, string> _user =
         new() {{"username", "TestUser123"}, {"email", "TestEmail@test.test"}, {"password", "TestPassword"}, {"CheepText", "Hello fellow students! 4283"}};
     private readonly string _publicUrl = "http://localhost:8080/";
     private readonly string _registerUrl = "http://localhost:8080/Identity/Account/Register";
     private readonly string _loginUrl = "http://localhost:8080/Identity/Account/Login";
+    private readonly string _aboutmeURL = $"http://localhost:8080/{_user["username"]}/AboutMe";
     
     [SetUp]
     public async Task SetUpAsync()
@@ -78,11 +79,35 @@ public class RegisterE2ETest : PageTest
         await Expect(_page.GetByText(_user["CheepText"])).ToBeVisibleAsync();
     }
 
+    [Test]
+    public async Task AboutMePageShouldBeAccessible()
+    {
+        await _page.GotoAsync(_aboutmeURL);
+        await Expect(_page.GetByRole(AriaRole.Button, new() { Name = "Forget Me" })).ToBeVisibleAsync();
+
+    }
+
     [TearDown]
     public async Task TearDownAsync()
     {
+        DeleteTestuser();
         await _browser.CloseAsync();
         _playwright.Dispose();
     }
+    
+    //Helper methods
+
+    public async void DeleteTestuser()
+    {
+        await _page.GotoAsync(_aboutmeURL);
+        void Page_Dialog_EventHandler(object sender, IDialog dialog)
+         {
+            Console.WriteLine($"Dialog message: {dialog.Message}");
+            dialog.DismissAsync();
+            _page.Dialog -= Page_Dialog_EventHandler;
+         }
+        _page.Dialog += Page_Dialog_EventHandler;
+        await _page.GetByRole(AriaRole.Button, new() { Name = "Forget Me" }).ClickAsync();
+    }
 }
-//pwsh bin/Debug/net8.0/playwright.ps1 codegen 
+//pwsh bin/Debug/net8.0/playwright.ps1 codegen {URL TO TEST ON}
